033f6d62c19461560469484eb70c5c45e2dbb2af
Don Cooper <don_cooper@hotmail.com>
2017-05-15 13:26:07 -0400

    Excercise 2

---
+++ excercise2.rb
@@ -0,0 +1,17 @@
+# How would you calculate a good tip for a 55 dollar meal?
+
+meal = 55
+tip = meal * 0.15
+puts tip
+
+# Try adding a string and an integer with the + operator
+
+puts "string" + 5.to_s
+
+# Try outputting the result of 45628 multiplied by 7839
+
+puts "A really large number would be #{45628 * 7839}"
+
+# What's the value of the expression...true
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
